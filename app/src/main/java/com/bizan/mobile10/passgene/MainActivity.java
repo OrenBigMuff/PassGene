416b0a8bc4851a396a574b842db055fe4876b6c6
syuheihori <syuhei09262@gmail.com>
2016-02-08 23:16:41 +0900

    content_scrollingの追加、Stringに2項目追加、ツールバーにNVのトグル追加
    Stringに：openDrawer,closeDrawerの２つ追加しました。
    ツールバーにトグル追加しました。今の段階ではクリックするとアプリが落ちます

--- app/src/main/java/com/bizan/mobile10/passgene/MainActivity.java
+++ app/src/main/java/com/bizan/mobile10/passgene/MainActivity.java
@@ -1,13 +1,57 @@
 package com.bizan.mobile10.passgene;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    String nvTITLES[]={"ユーザー情報設定","アプリ設定","バックアップ"};    //NV内のメニュー
+//    int nvICONS = {};                                             //NV内のメニューアイコン
+
+    String nvSETTING = "設定画面";                                    //ヘッダービュー内の”設定”の文字
+    String useNAME = "";                                             //ユーザーネーム
+
+//    int HEADERVIEW = ;                                             //ヘッダーのビュー
+
+    private Toolbar maToolbar;       //toolbar
+    RecyclerView maRecycleView;      //リサイクルビュー
+    RecyclerView.Adapter maAdapter;   //リサイクルビューアダプター
+    RecyclerView.LayoutManager maLayoutManager;      //リサイクルビューレイアウトマネージャー
+    DrawerLayout maDrawer;           //ドロワー
+
+    ActionBarDrawerToggle maDrawerToggle;            //NVを開くためのトグル
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //アクションバーの代わりにツールバーを設定
+        maToolbar = (Toolbar) findViewById(R.id.MainActivity_toolbar);
+        setSupportActionBar(maToolbar);
+
+        //ツールバーにNVトグルを追加
+        maDrawer = (DrawerLayout) findViewById(R.id.MainActivity_DrawerLayout);
+        maDrawerToggle = new ActionBarDrawerToggle(this,maDrawer,maToolbar,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+        public void onDrawerOpened(View drawerView){
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+        public void onDrawerClosed(View drawerView){
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        maDrawer.setDrawerListener(maDrawerToggle);
+        maDrawerToggle.syncState();
+        //ここまでNVトグル設定
+
+
     }
 }
