e37768e51109f7cd979c70f4c832e0a2407e6e47
Dan_Imai <oinarhythm0508@gmail.com>
2016-02-10 03:02:06 +0900

    InitialSet1のコードを整理
    未使用のimportを削除

--- app/src/main/java/com/bizan/mobile10/passgene/InitialSet1.java
+++ app/src/main/java/com/bizan/mobile10/passgene/InitialSet1.java
@@ -1,20 +1,41 @@
 package com.bizan.mobile10.passgene;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.Button;
 
-public class InitialSet1 extends AppCompatActivity {
+public class InitialSet1 extends AppCompatActivity
+        implements View.OnClickListener {
 
+    Button btn;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_set1);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btn = (Button) findViewById(R.id.btnInitialSet1);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(InitialSet1.this, InitialSet2.class);
+        startActivity(intent);
 
     }
 }
