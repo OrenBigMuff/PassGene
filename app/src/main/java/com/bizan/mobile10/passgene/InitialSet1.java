c87f0c5f7102c3eb1a2a85413eaf881627f2b4b4
Dan_Imai <oinarhythm0508@gmail.com>
2016-02-12 09:19:37 +0900

    InitialSet1（P0-1）の更新
    必須項目を未入力時のエラーメッセージを表示するように変更
    Stringでエラーメッセージを指定

--- app/src/main/java/com/bizan/mobile10/passgene/InitialSet1.java
+++ app/src/main/java/com/bizan/mobile10/passgene/InitialSet1.java
@@ -8,20 +8,25 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.Layout;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewAnimationUtils;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InitialSet1 extends AppCompatActivity
         implements View.OnClickListener {
 
     Button btn;
-    TextInputLayout inputLayoutL;
-    TextInputLayout inputLayoutF;
-    EditText inputLastname;
-    EditText inputFirstname;
+    private TextInputLayout inputLayoutL;
+    private TextInputLayout inputLayoutF;
+    private EditText inputLastname;
+    private EditText inputFirstname;
+
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -35,6 +40,9 @@ public class InitialSet1 extends AppCompatActivity
         btn = (Button) findViewById(R.id.btnInitialSet1);
         btn.setOnClickListener(this);
 
+        inputLastname = (EditText)findViewById(R.id.inputLastname);
+        inputFirstname = (EditText)findViewById(R.id.inputFirstname);
+
         inputLayoutL = (TextInputLayout) findViewById(R.id.inputUserLastname);
         inputLayoutL.setError("姓をアルファベットで入力して下さい。"); // show error
         inputLayoutL.setError(null); // hide error
@@ -43,19 +51,105 @@ public class InitialSet1 extends AppCompatActivity
         inputLayoutF.setError("名をアルファベットで入力して下さい。"); // show error
         inputLayoutF.setError(null); // hide error
 
+        inputLastname.addTextChangedListener(new PGTextWatcher(inputLayoutL));
+        inputFirstname.addTextChangedListener(new PGTextWatcher(inputLayoutF));
 
+
+    }
+
+    /**
+     * Validating form
+     */
+    private void submitForm() {
+        if (!validateLastname()) {
+            return;
+        }
+
+        if (!validateFirstname()) {
+            return;
+        }
+        String tmpLast = inputLastname.getText().toString();
+        String tmpFirst = inputFirstname.getText().toString();
+        toast("はじめまして" + tmpLast + " " + tmpFirst +"さん、\n次はマスターパスワードを決めてください。");
+        Intent intent = new Intent(InitialSet1.this, InitialSet2.class);
+        startActivity(intent);
+    }
+
+    private boolean validateLastname() {
+        if (inputLastname.getText().toString().trim().isEmpty()) {
+            inputLayoutL.setError(getString(R.string.err_msg_lastname));
+            requestFocus(inputLastname);
+            return false;
+        } else {
+            inputLayoutL.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateFirstname() {
+        if (inputFirstname.getText().toString().trim().isEmpty()) {
+            inputLayoutF.setError(getString(R.string.err_msg_firstname));
+            requestFocus(inputFirstname);
+            return false;
+        } else {
+            inputLayoutF.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class PGTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private PGTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.inputUserLastname:
+                    validateLastname();
+                    break;
+                case R.id.inputUserFirstname:
+                    validateFirstname();
+                    break;
+            }
+        }
     }
 
     @Override
     public void onClick(View v) {
-        inputLastname = (EditText) findViewById(R.id.inputLastname);
-        inputFirstname = (EditText) findViewById(R.id.inputFirstname);
 
-        if (inputLastname.getText().toString().equals("") || inputFirstname.getText().toString().equals("")) {
-//アラートダイアログを出すコード
-        }else {
-            Intent intent = new Intent(InitialSet1.this, InitialSet2.class);
-            startActivity(intent);
+
+        submitForm();
+
+
+
+    }
+
+    /**
+     * あったら便利！トーストメソッドだよ
+     *
+     * @param text
+     */
+    private void toast(String text) {
+        if (text == null) {
+            text = "";
         }
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
