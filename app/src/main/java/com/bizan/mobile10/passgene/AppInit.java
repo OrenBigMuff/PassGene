921353f437be1f629662c02f19c3b22a82ec3bd2
syuheihori <syuhei09262@gmail.com>
2016-02-16 09:52:34 +0900

    AppInitのjavaファイルとレイアウトファイル追加

---
+++ app/src/main/java/com/bizan/mobile10/passgene/AppInit.java
@@ -0,0 +1,15 @@
+package com.bizan.mobile10.passgene;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2016/02/16.
+ */
+public class AppInit extends AppCompatActivity{
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appinit);
+    }
+}
