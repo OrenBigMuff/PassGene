13ef0f6169693b5e5adcbd645faf683f4c03a50e
syuheihori <syuhei09262@gmail.com>
2016-03-07 15:58:27 +0900

    mpChangeクラスコーディング、データの書き換えはまだです

--- app/src/main/java/com/bizan/mobile10/passgene/MPchange.java
+++ app/src/main/java/com/bizan/mobile10/passgene/MPchange.java
@@ -1,16 +1,88 @@
 package com.bizan.mobile10.passgene;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Toolbar;
 
 /**
  * Created by user on 2016/03/07.
  */
-public class MPchange extends AppCompatActivity{
+public class MPchange extends AppCompatActivity implements MPchangeDialog.DialogListener, View.OnClickListener {
+
+    public static DatabaseC dbC;
+    private PreferenceC pref;
+    private NumberPicker npk1;
+    private NumberPicker npk2;
+    private NumberPicker npk3;
+    private NumberPicker npk4;
+    private Button btn;
+    private String newpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mpchange);
+
+        dbC = new DatabaseC(PassList2.getDbHelper());
+
+        btn = (Button) findViewById(R.id.btnMPchange);
+        btn.setOnClickListener(this);
+
+        npk1 = (NumberPicker) findViewById(R.id.mpcnpk1);
+        npk2 = (NumberPicker) findViewById(R.id.mpcnpk2);
+        npk3 = (NumberPicker) findViewById(R.id.mpcnpk3);
+        npk4 = (NumberPicker) findViewById(R.id.mpcnpk4);
+
+        npk1.setMinValue(0);
+        npk1.setMaxValue(9);
+        npk2.setMinValue(0);
+        npk2.setMaxValue(9);
+        npk3.setMinValue(0);
+        npk3.setMaxValue(9);
+        npk4.setMinValue(0);
+        npk4.setMaxValue(9);
     }
+
+    @Override
+    public void onClick(View v) {
+        //Num Pickerから数字を取得
+        String tmp1 = String.valueOf(npk1.getValue());
+        String tmp2 = String.valueOf(npk2.getValue());
+        String tmp3 = String.valueOf(npk3.getValue());
+        String tmp4 = String.valueOf(npk4.getValue());
+        newpass = tmp1 + tmp2 + tmp3 + tmp4;
+
+        //Dialogを表示させる
+        openPG_Dialog();
+    }
+
+    private void openPG_Dialog() {
+        //DialogFragmentに渡すモノを決めてね
+        String title = "マスターパスワード変更";
+        String message ="マスターパスワードを" + newpass + "に変更します。\nよろしいですか？";
+        String posi = "変更";
+        String nega = "戻る";
+        //ダイアログのレイアウトResId
+        int resId_dialog = R.layout.fragment_mpchange_dialog;
+
+        FragmentManager fm = getSupportFragmentManager();
+        MPchangeDialog alertDialog = MPchangeDialog.newInstance(title, message, posi, nega, resId_dialog);
+        alertDialog.show(fm, "fragment_alert");
+    }
+
+    @Override
+    public void onPositiveButtonClick(DialogFragment dialog) {
+
+    }
+
+    @Override
+    public void onNegativeButtonClick(DialogFragment dialog) {
+
+    }
+
 }
---
+++ app/src/main/java/com/bizan/mobile10/passgene/MPchangeDialog.java
@@ -0,0 +1,125 @@
+package com.bizan.mobile10.passgene;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by user on 2016/03/07.
+ */
+public class MPchangeDialog extends DialogFragment{
+
+    TextView title_mcdl;
+    TextView message_mcdl;
+
+    //コンストラクタ
+    public MPchangeDialog(){
+
+    }
+    /**
+     * インスタンス生成とsetArguments
+     *
+     * @return
+     */
+    public static MPchangeDialog newInstance(String title, String message,String posi, String nega, int resId) {
+        MPchangeDialog instance = new MPchangeDialog();
+
+        // ダイアログに渡すパラメータはBundleにまとめる
+        Bundle args = new Bundle();
+
+        args.putString("title", title);             //Title
+        args.putString("message", message);         //message
+        args.putString("posi", posi);               //posi
+        args.putString("nega", nega);               //nega
+        args.putInt("resId",resId);   //resId_dialog layout
+
+        instance.setArguments(args);
+        return instance;
+    }
+    /**
+     * activityにアタッチされていなければ使えないリスナー
+     * @param activity
+     */
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            pListener = (DialogListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + "must implements DialogListener");
+        }
+    }
+
+    private DialogListener pListener = null;
+
+    public interface DialogListener{
+
+
+        void onPositiveButtonClick(DialogFragment dialog);
+
+        void onNegativeButtonClick(DialogFragment dialog);
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+
+        //値を受け取る
+        String title = getArguments().getString("title");
+        String message = getArguments().getString("message");
+        int layoutId = getArguments().getInt("resId");        //今後の機能拡張の為に置いときます
+        String posi = getArguments().getString("posi");
+        String nega = getArguments().getString("nega");
+
+        Dialog dialog = new Dialog(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View view1 = inflater.inflate(R.layout.fragment_mpchange_dialog,null);
+
+        //ダイアログのタイトルを取得
+        title_mcdl = (TextView)view1.findViewById(R.id.title_mcdl);
+        title_mcdl.setText(title);
+        //ダイアログのメッセージを取得
+        message_mcdl = (TextView)view1.findViewById(R.id.message_mcdl);
+        message_mcdl.setText(message);
+
+        //Positiveボタンを実装
+        Button pb = (Button) view1.findViewById(R.id.positive_button_mpchange);
+        pb.setText(posi);
+        pb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pListener.onPositiveButtonClick(MPchangeDialog.this);
+            }
+        });
+
+        //Negativeボタンを実装
+        Button nb = (Button) view1.findViewById(R.id.close_button_mpchange);
+        nb.setText(nega);
+        nb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pListener.onNegativeButtonClick(MPchangeDialog.this);
+            }
+        });
+        //ダイアログの背景とか消す処理
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);         //タイトル非表示
+        dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);      //フルスクリーン
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));         //背景を透明にする
+
+        //Viewをセットする
+        dialog.setContentView(view1);
+
+        return dialog;
+    }
+
+}
